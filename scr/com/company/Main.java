27e735c12ab79f9003f6cc8ae3dbf03db8d76601
shlkeyboardhero <daniil.shelkovyy@yandex.ru>
2020-10-24 22:35:48 +0300

    Создание Main и Dice

---
+++ scr/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+// write your code here
+
+        Dice diceN1 = new Dice();
+        Dice diceN2 = new Dice();
+
+        diceN1.getDiceNumber();
+        diceN1.getDiceNumber();
+    }
+
+}
