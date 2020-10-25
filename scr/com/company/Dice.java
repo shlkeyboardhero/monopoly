4eb5eeab9ff90c2d0751471adbc0efaa62bc7958
shlkeyboardhero <daniil.shelkovyy@yandex.ru>
2020-10-25 22:51:20 +0300

    Исправление

--- scr/com/company/Dice.java
+++ scr/com/company/Dice.java
@@ -3,13 +3,12 @@ package com.company;
 
 public class Dice {
 
-    int dice;
 
-    public void getDiceNumber() {
-        this.dice = getRandom();
+    public int getDiceNumber() {
+        return getRandom();
     }
 
-    public int getRandom(){
+    public int getRandom() {
         int rnd;
         rnd = (int) (1 + Math.random() * 5);
         return rnd;
--- scr/com/company/Main.java
+++ scr/com/company/Main.java
@@ -3,13 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-// write your code here
 
         Dice diceN1 = new Dice();
         Dice diceN2 = new Dice();
 
-        diceN1.getDiceNumber();
-        diceN1.getDiceNumber();
+        int dice1 = diceN1.getDiceNumber();
+        int dice2 = diceN2.getDiceNumber();
+
+        System.out.println(dice1);
+        System.out.println(dice2);
     }
 
 }
