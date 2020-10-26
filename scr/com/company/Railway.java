ffd49d45615500e3a0d96e0feb35a4eb4cb6d804
shlkeyboardhero <daniil.shelkovyy@yandex.ru>
2020-10-26 13:16:30 +0300

    Add

---
+++ scr/com/company/Railway.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Railway {
+    String name;
+    int cost;
+    int rent;
+
+    public Railway(String name, int cost) {
+        this.name=name;
+        this.cost=cost;
+    }
+}
---
+++ scr/com/company/Street.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Street {
+    String name;
+    int rent;
+    int cost;
+
+    public Street(String name, int cost){
+        this.name=name;
+        this.cost=cost;
+    }
+}
